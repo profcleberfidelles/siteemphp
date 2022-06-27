469b6f27593588e5e26cc2df7ba7e89e8301e413
profcleberfidelles <108277066+profcleberfidelles@users.noreply.github.com>
2022-06-27 09:32:09 -0300

    arquivo inicial do projeto

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $nota1 = 2;
+    $nota2 = 2;
+    $media = ($nota1+$nota2)/2;
+
+    if($media>=6):
+        echo "o aluno foi aprovado";
+    
+    else:
+        echo 'o aluno foi reprovado';
+    endif;
+?>
